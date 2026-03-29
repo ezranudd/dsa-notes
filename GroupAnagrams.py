2db8eb66c49587eaab703952bbb571dfb5b7df1e
nudd <nudd@wisc.edu>
2026-03-29 09:59:41 -0500

    Group Anagrams

---
+++ GroupAnagrams.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    # Time: O(m*nlogn)
+    # Space: O(m*n)
+    def groupAnagramsSorting(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
+
+    # Time: O(m*n)
+    # Space: O(m*n)
+    def groupAnagramsHash(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
