19836d59a8b522d949c365fdbeedd65877297fb6
nudd <nudd@wisc.edu>
2026-03-31 19:56:14 -0500

    Top K Frequent

---
+++ TopKFrequent.py
@@ -0,0 +1,48 @@
+from typing import List
+import heapq
+
+class Solution:
+
+    #O(n + mlogm)
+    def topKFrequentSort(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for i in nums:
+            count[i] = 1 + count.get(i, 0)
+        return sorted(count, key=count.get, reverse=True)[:k]
+
+    #O(nlogk)
+    # neetcode.io min heap solution
+    def topKFrequentMinHeap(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+
+    # Time: O(n)
+    # Space: O(n)
+    # neetcode.io bucket sort solution
+    def topKFrequentBucket(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
